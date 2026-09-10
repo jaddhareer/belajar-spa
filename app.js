48a93335be74791a0a1408223fc90583e5962a9f
jaddhareer <jaddhareer@gmail.com>
2026-09-10 19:34:18 +0700

    first commit, SPA learning

---
+++ app.js
@@ -0,0 +1,26 @@
+const barang = [
+    {nama: 'Pulpen', stok: '20'},
+    {nama: 'Buku', stok: '50'},
+    {nama: 'Pensil', stok: '20'}
+];
+
+const container = document.querySelector('#daftar-barang');
+const list = document.querySelectorAll('li');
+
+list.forEach(element => {
+    element.textContent = "Barang A"
+});
+
+container.addEventListener('click', function(e) {
+    if(e.target.tagName === 'LI') {
+        const detail = document.getElementById('detail');
+        detail.textContent = e.target.textContent;
+        console.log(e.target)
+    }
+})
+
+barang.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = `${item.nama} (Stok: ${item.stok})`;
+    container.appendChild(li);
+});
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Single Page Aplication</title>
+</head>
+<body>
+    <h2>Daftar Barang</h2>
+    <ul id="daftar-barang">
+    </ul>
+    <div id="detail"></div>
+    <script src="app.js">
+    </script>
+</body>
+</html>
